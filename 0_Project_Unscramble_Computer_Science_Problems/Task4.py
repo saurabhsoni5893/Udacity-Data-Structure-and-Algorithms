9038b5a66eccfdf191ec40c7bf70da3e3fba002c
saurabhsoni589trtrtt <saurabhsoni5893@gmail.com>
2020-06-02 19:25:25 -0500

    Create Task4.py

---
+++ 0_Project_Unscramble_Computer_Science_Problems/Task4.py
@@ -0,0 +1,42 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 4:
+The telephone company want to identify numbers that might be doing
+telephone marketing. Create a set of possible telemarketers:
+these are numbers that make outgoing calls but never send texts,
+receive texts or receive incoming calls.
+
+Print a message:
+"These numbers could be telemarketers: "
+<list of numbers>
+The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"""
+
+dials = {call[0] for call in calls}
+receives = [call[1] for call in calls]
+real_people = set()
+
+for text, receive in zip(texts, receives):
+    for i in range(0, 2):
+        real_people.add(text[i])
+    real_people.add(receive)
+
+potential_telemarketers = {call for call in dials if call not in real_people}
+potential_telemarketers = sorted(list(potential_telemarketers))
+
+print(f"These numbers could be telemarketers: {potential_telemarketers}")
+print(f"Total potential telemarketers: {len(potential_telemarketers)}")
+
